414bf2b07cf1d9e69ad94ac30eda2ae83bf7f5e0
uzairmuzaffer <uzairmuzaffer67@gmail.com>
2024-12-04 21:09:17 +0500

    added solana wallet with Vue and integrated Honeycomb Protocol's profile functionality.

--- src/hpl.ts
+++ src/hpl.ts
@@ -1,10 +1,12 @@
 // eslint-disable
 import createEdgeClient from "@honeycomb-protocol/edge-client";
 
-export const DAS_RPC = process.env.NEXT_PUBLIC_RPC_ENDPOINT || "https://rpc.test.honeycombprotocol.com";
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://edge.test.honeycombprotocol.com";
-export const HPL_PROJECT_PUBLIC_KEY = "Gj6UaodRwTkQGD8pPjxMPm3nNSbQbobjcq1nxZv2aekQ";
-export const pfp = "https://www.arweave.net/1MDsAxGaezXbo80mZqEOZrub-nvVDxXccRNo-gNib4U?ext=png";
+export const DAS_RPC =
+  process.env.RPC_ENDPOINT || "https://rpc.test.honeycombprotocol.com";
+export const API_URL =
+  process.env.API_URL || "https://edge.test.honeycombprotocol.com";
+export const HPL_PROJECT_PUBLIC_KEY =
+  "Gj6UaodRwTkQGD8pPjxMPm3nNSbQbobjcq1nxZv2aekQ";
+export const pfp =
+  "https://www.arweave.net/1MDsAxGaezXbo80mZqEOZrub-nvVDxXccRNo-gNib4U?ext=png";
 export const edgeClient = createEdgeClient(API_URL, true);
-
-console.log("edgeClient:", edgeClient);
